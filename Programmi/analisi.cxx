4763c4d432cc6ea39186ff2912fabc635642edf4
Tommaso Bertola <bertola.tommaso@hotmail.it>
2020-03-18 22:46:41 +0100

    Nomi più precisi per variabili e vettori

--- Programmi/analisi.cxx
+++ Programmi/analisi.cxx
@@ -13,6 +13,9 @@ int main()
     string dir_ac, sub_dir, alto_basso;
     double t;
     vector<double> media_1_acq, dstd_1_acq;
+    string numero;
+    double media_vett;
+    vector<double> dati_file; // creo un vettore dove immagazzinare i 3 dati per ogni foglio e faccio fare la media con libreria
     vector<double> dinamometro;
     for (int i = 0; i < 1600; i = i + 100)
     {
@@ -27,39 +30,35 @@ int main()
         cout << "Vuoi legge i file di Allungamento o Accorciamento? (all / acc): ";
         cin >> sub_dir;
 
-        for (int i = 200; i < 1300; i = i + 100)
+        for (int i = 200; i < 1300; i = i + 100) //legge tutti i file 200->1200
         {
-            string numero;
-            double media_vett;
             numero = to_string(i); //trasformo (i) in una stringa chiamata numero
-            vector<double> primo;  // creo un vettore dove immagazzinare i 3 dati per ogni foglio e faccio fare la media con libreria
-
             ifstream fin("../" + dir_ac + "/" + sub_dir + "/" + numero + ".txt");
             if (!fin) //controllo lettura
             {
                 cout << "Errore di lettura" << endl;
                 return 1;
             }
-
             while (fin >> t)
             {
-                primo.push_back(t);
+                dati_file.push_back(t);
             }
-
-            media_1_acq.push_back(media(primo));
-            dstd_1_acq.push_back(dstd_media(primo));
+            media_1_acq.push_back(media(dati_file));
+            dstd_1_acq.push_back(dstd_media(dati_file));
         }
         ofstream fout("grafico.txt");
 
         for (int i = 0; i < media_1_acq.size(); i++)
         {
             cout << dinamometro[i] << "\t" << media_1_acq[i] << "\t" << dstd_1_acq[i] << endl;
+            fout << "#Forza[g_peso]\tDelta_x[1e-6m]\tDstd[1e-6m]" << endl; //Stampa descrizione colonne per rendere migliore la lettura del file all'utilizzatore finale
             fout << dinamometro[i] << "\t" << media_1_acq[i] << "\t" << dstd_1_acq[i] << endl;
         }
         cout << "Coeff angolare: " << b_angolare(dinamometro, media_1_acq) << "+-" << sigma_b(dinamometro, media_1_acq) << endl;
         cout << "Intercetta: " << a_intercetta(dinamometro, media_1_acq) << "+-" << sigma_a(dinamometro, media_1_acq) << endl;
     }
-    if (dir_ac == "2ac") //legge 2 acquisizione
+
+    if (dir_ac == "2ac") //legge 2 acquisizione QUasi sicuramente si può muovere in un altro file
     {
         vector<double> temp;
         vector<double> media_2_acq, dstd_2_acq;
