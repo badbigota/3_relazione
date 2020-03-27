8da9eadab539b2db4622e1d338b3ab449df00189
Tommaso Bertola <bertola.tommaso@hotmail.it>
2020-03-27 12:32:44 +0100

    Compatibilità 1ac allungamento e accorciamento

---
+++ Programmi/compatib_1ac_all_acc.cxx
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include "statistica.h"
+
+using namespace std;
+
+int main()
+{
+    string file = "comp_1ac.txt";
+    ifstream fin(file);
+    double b_an, si_an, a_in, si_in;
+    vector<double> b_ang;
+    vector<double> s_b;
+    vector<double> a_int;
+    vector<double> s_a;
+    while (fin >> b_an>>si_an>>a_in>>si_in)
+    {
+        b_ang.push_back(b_an);
+        s_b.push_back(si_an);
+        a_int.push_back(a_in);
+        s_a.push_back(si_in);
+    }
+    cout<<"Media ponderata:\t"<<media_ponderata(b_ang, s_b)<<" +/- "<<errore_media_ponderata(s_b)<<endl;
+    cout<<"Compatibilità fra i 2 K all e acc:\t"<<comp_3(b_ang[0],b_ang[1],s_b[0],s_b[1])<<endl;
+}
